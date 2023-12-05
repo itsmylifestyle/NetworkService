deace0f9d5745d319159044c2ad4bd5c05f7fa4b
itsmylifestyle <hahaii00@mail.ru>
2023-12-05 22:29:38 +0600

    Initial Commit

---
+++ INS/INSApp.swift
@@ -0,0 +1,17 @@
+//
+//  INSApp.swift
+//  INS
+//
+//  Created by Айбек on 05.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct INSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
